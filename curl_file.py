092c8ec29375e86e338e56bf2cfa78fc55da0986
Vitaly Makhaev <pterik@gmail.com>
2023-05-10 04:35:06 -0400

    Commit

--- curl_file.py
+++ curl_file.py
@@ -1,6 +1,8 @@
 #crawl the page https://index.minfin.com.ua/ua/russian-invading/casualties/
 
 from urllib.request import Request, urlopen
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 #url = 'https://index.minfin.com.ua/ua/russian-invading/casualties/'
 req = Request(
     url='https://index.minfin.com.ua/ua/russian-invading/casualties/', 
