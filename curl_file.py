ccefbdd18973ab3aa25efdf52763b397eab2051a
Vitaly Makhaev <pterik@gmail.com>
2023-05-10 04:16:45 -0400

    111

--- curl_file.py
+++ curl_file.py
@@ -1,1 +1,10 @@
 #crawl the page https://index.minfin.com.ua/ua/russian-invading/casualties/
+
+from urllib.request import Request, urlopen
+#url = 'https://index.minfin.com.ua/ua/russian-invading/casualties/'
+req = Request(
+    url='https://index.minfin.com.ua/ua/russian-invading/casualties/', 
+    headers={'User-Agent': 'Mozilla/5.0'}
+)
+webpage = urlopen(req).read()
+print(webpage)
